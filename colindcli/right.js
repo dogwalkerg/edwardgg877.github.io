4d1be1bb5454ae2d5352c5c02218e2c4e124a123
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2024-10-25 23:13:03 +0800

    add file

---
+++ colindcli/right.js
@@ -0,0 +1,8 @@
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:inline-block;width:200px;height:200px\'");
+document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
+document.writeln("     data-ad-slot=\'1320706181\'></ins>");
+
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
